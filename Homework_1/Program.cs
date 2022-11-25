47de747cfbe4c999c472c3de052f9f622f4d541e
Alarikii <alarikhawk@gmail.com>
2022-11-25 23:30:14 +0900

    complete Task 8

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -65,3 +65,26 @@ else
 }
 */
 
+
+
+/*
+//Задача 8: Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число больше 0");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (number > 0)
+{
+    while (count <= number)
+    {
+        if (count % 2 == 0)
+        {
+            Console.Write($"{count} ");
+        }
+        count += 1;
+    }
+}
+else
+{
+    Console.WriteLine("Неверное число");
+}
+*/
