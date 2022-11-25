a58beaa8dd797cd74ea707ee83b77c4c2432573f
Alarikii <alarikhawk@gmail.com>
2022-11-25 23:03:54 +0900

    complete Task 2, 4, 6

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,17 +1,67 @@
-﻿Console.WriteLine("Введите первое число");
+﻿/*
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
 int numberFirst = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int numberSecond = Convert.ToInt32(Console.ReadLine());
 int max;
+int min;
 if (numberFirst > numberSecond)
 {
     max = numberFirst;
+    min = numberSecond;
 }
 else
 {
     max = numberSecond;
+    min = numberFirst;
 }
 Console.Clear();
 Console.WriteLine("a = " + numberFirst);
 Console.WriteLine("b = " + numberSecond);
 Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
+*/
+
+
+
+/*
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberThird = Convert.ToInt32(Console.ReadLine());
+int max = numberFirst;
+if (max < numberSecond)
+{
+    max = numberSecond;
+}
+if (max < numberThird)
+{
+    max = numberThird;
+}
+Console.Clear();
+Console.WriteLine($"{numberFirst}, {numberSecond}, {numberThird}");
+Console.WriteLine("max = " + max);
+*/
+
+
+
+/*
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+*/
+
