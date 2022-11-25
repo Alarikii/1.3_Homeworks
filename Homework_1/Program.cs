693f00563132d39989bc7822765b8a21b170fd6b
Alarikii <alarikhawk@gmail.com>
2022-11-25 22:36:02 +0900

    complete Task 2

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("Введите первое число");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+int max;
+if (numberFirst > numberSecond)
+{
+    max = numberFirst;
+}
+else
+{
+    max = numberSecond;
+}
+Console.Clear();
+Console.WriteLine("a = " + numberFirst);
+Console.WriteLine("b = " + numberSecond);
+Console.WriteLine("max = " + max);
