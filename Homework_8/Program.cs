c12b472cd3dd18524bcdb9bb42a40caca00fdefd
Alarikii <alarikhawk@gmail.com>
2023-01-09 22:40:43 +0900

    add Homework_8 and Homework_9

---
+++ Homework_8/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace Homework_8;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Задача 54: Задайте двумерный массив. Напишите программу, которая
+        // упорядочит по убыванию элементы каждой строки двумерного массива.
+        void Task54()
+        {
+            Console.WriteLine("=====  Задача 54  =====");
+            int columns = 4;
+            int rows = 4;
+            int[,] numbers = new int[rows, columns];
+            FillArray(numbers);
+            PrintArray(numbers);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int count = 0; count < columns - 1; count++)
+                {
+                    for (int j = 0; j < columns - count - 1; j++)
+                    {
+                        if (numbers[i, j] > numbers[i, j + 1])
+                        {
+                            int temp = numbers[i, j];
+                            numbers[i, j] = numbers[i, j + 1];
+                            numbers[i, j + 1] = temp;
+                        }
+                    }
+                }
+            }
+            PrintArray(numbers, "Упорядоченный массив:");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        //Задача 56: Задайте прямоугольный двумерный массив. Напишите
+        // программу, которая будет находить строку с наименьшей суммой элементов.
+        void Task56()
+        {
+            Console.WriteLine("=====  Задача 56  =====");
+            int rows = 4;
+            int columns = 5;
+            int[,] numbers = new int[rows, columns];
+            FillArray(numbers, 0, 5);
+            PrintArray(numbers);
+
+            int sum1 = 0;
+            int sum2 = 0;
+            int minSumIndex = 0;
+            for (int i = 1; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    sum1 = sum1 + numbers[minSumIndex, j];
+                }
+                for (int j = 0; j < columns; j++)
+                {
+                    sum2 = sum2 + numbers[i, j];
+                }
+                if (sum1 > sum2) {minSumIndex = i;}
+                sum1 = 0;
+                sum2 = 0;
+            }
+            Console.WriteLine("Индекс строки с наименьшей суммой элементов: " + minSumIndex);
+            
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        Task54();
+        Task56();
+
+    }
+    static void FillArray(int[,] numbers, int minValue = 0, int maxValue = 9)
+    {
+        maxValue++;
+        Random rand = new Random();
+        int rows = numbers.GetLength(0);
+        int columns = numbers.GetLength(1);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                numbers[i, j] = rand.Next(minValue, maxValue);
+            }
+        }
+    }
+
+    static void PrintArray(int[,] numbers, string message = "Вывод массива: ")
+    {
+        int rows = numbers.GetLength(0);
+        int columns = numbers.GetLength(1);
+        Console.WriteLine(message);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(numbers[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Homework_9/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace Homework_9;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Задача 64: Задайте значения M и N. Напишите рекурсивный метод, который
+        // выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+        void Task64()
+        {
+            Console.WriteLine("=====  Задача 64  =====");
+            Console.WriteLine("Введите нижнюю границу интервала (натуральное число)");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите верхнюю границу интервала (натуральное число)");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (0 <= num1 && 0 <= num2)
+            {
+                Console.WriteLine($"Числа кратные 3 в промежутке [{num1}, {num2}]: ");
+                DigitsRowOf3(num1, num2);
+            }
+            else Console.WriteLine("Введено не натуральное число");
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        // Задача 66: Задайте значения M и N. Напишите рекурсивный метод, который
+        // найдёт сумму натуральных элементов в промежутке от M до N.
+        void Task66()
+        {
+            Console.WriteLine("=====  Задача 66  =====");
+            Console.WriteLine("Введите нижнюю границу интервала (натуральное число)");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите верхнюю границу интервала (натуральное число)");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (0 <= num1 && 0 <= num2)
+            {
+                Console.WriteLine($"Сумма чисел в промежутке [{num1}, {num2}]: {SumRow(num1, num2)}");
+
+            }
+            else Console.WriteLine("Введено не натуральное число");
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        // Задача 68: Напишите программу вычисления функции Аккермана с помощью
+        // рекурсии. Даны два неотрицательных числа m и n.
+        void Task68()
+        {
+            Console.WriteLine("=====  Задача 68  =====");
+            Console.WriteLine("Введите первое число натуральное число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе натуральное число)");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (0 <= num1 && 0 <= num2)
+            {
+                Console.WriteLine($"Функция Аккермана A({num1}, {num2}): {Akkerman(num1, num2)}");
+
+            }
+            else Console.WriteLine("Введено не натуральное число");
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        Task64();
+        Task66();
+        Task68();
+
+    }
+    static void DigitsRowOf3(int num1, int num2)
+    {
+        if (num1 > num2) return;
+        if (num1 % 3 == 0)
+        {
+            Console.Write(num1 + "  ");
+        }
+        num1++;
+        DigitsRowOf3(num1, num2);
+    }
+
+    static int SumRow(int num1, int num2, int sum = 0)
+    {
+        if (num1 > num2) return sum;
+        sum += num1;
+        num1++;
+        return SumRow(num1, num2, sum);
+    }
+
+    static int Akkerman(int m, int n)
+    {
+        if(m == 0) return(n+1);
+        else if (n == 0) {return Akkerman(m-1, 1);}
+        else {return Akkerman(m-1, Akkerman(m, n-1));}
+    }
+}
